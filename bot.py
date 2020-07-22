6b771ab4c9804f3ec781f53f72617e1ab35b8675
= <michael.w.murphy@outlook.com>
2020-07-22 09:15:07 +0100

    🖼️ Now sends a pre-defined chip image for sobriety of 365 days or less

--- bot.py
+++ bot.py
@@ -16,12 +16,47 @@ class TweetStreamListener(tweepy.StreamListener):
     def on_status(self, tweet):
         logger.info(f"Tweeting to {tweet.user.name} ({tweet.user.screen_name})")
 
-        self.reply_to_tweet(tweet)
-        # self.like_tweet(tweet)
+        if tweet.in_reply_to_status_id is not None or tweet.user.id == self.me.id:
+            logger.info("Ignoring reply to or own tweet")
+            return
+
+        days = self.get_duration_in_days(tweet)
+        self.like_tweet(tweet)
+        self.reply_to_tweet(tweet, days)
 
     def on_error(self, status):
         logger.error("There was an error with the stream listener")
         return False
+    
+    def get_duration_in_days(self, tweet) -> int or None:
+        """
+        Determines if the tweet has a specified sobriety duration
+        Returns the length of sobriety in days
+
+        Returns:
+            int or None
+
+        """
+        regex = re.compile("([1-9][0-9]{0,2}) (days|weeks|months|years)", re.IGNORECASE)
+        match = regex.search(tweet.text)
+
+        if match:
+            number, duration = match.group().split(" ")
+
+            durations = {
+                "day": 1,
+                "days": 1,
+                "week": 7,
+                "weeks": 7,
+                "month": 30.4161,
+                "months": 30.4161,
+                "year": 365,
+                "years": 365
+            }
+
+            return round(int(number) * durations.get(duration, 1))
+        else:
+            return None
 
     def like_tweet(self, tweet):
         if not tweet.favorited:
@@ -30,31 +65,26 @@ class TweetStreamListener(tweepy.StreamListener):
             except Exception:
                 logger.error("Error favouriting tweet")
 
-    def reply_to_tweet(self, tweet):
-        # Ignore replies and own tweets
-        if tweet.in_reply_to_status_id is not None or tweet.user.id == self.me.id:
-            return
-
+    def reply_to_tweet(self, tweet, days=None):
         # TODO Remove - only for testing
         test_accounts = get_config()["TESTING_ACCOUNTS"]
         if tweet.user.screen_name not in test_accounts:
             return
+
+        media_ids = []
+        if days is not None:
+            chip = Chip(days)
+            media_obj = self.api.media_upload(chip.path)
+            media_ids.append(media_obj.media_id)
         
-        regex = re.compile("([1-9][0-9]{0,2}) (days|weeks)", re.IGNORECASE)
-        match = regex.search(tweet.text)
-        number, duration = match.group().split(" ")
-
-        chip = Chip(number)
-
-        media_obj = self.api.media_upload(chip.path)
-
         self.api.update_status(
-            status=f"@{tweet.user.screen_name} Congratulations!",
-            media_ids=[media_obj.media_id],
+            status=f"Congratulations @{tweet.user.screen_name}!",
+            media_ids=media_ids,
             in_reply_to_status_id=tweet.id
         )
 
-        chip.delete()
+        if days is not None:
+            chip.delete()
 
 
 def main():
--- chip.py
+++ chip.py
@@ -15,32 +15,51 @@ class Chip:
     A representation of a sobriety chip
 
     Attributes:
+        days (int): The number of days associated with the chip
         number (str): The number associated with the Chip
         path (str): The path to the Chips temporary image location
-        image_exists (bool): True if the temporary image exists, otherwise False
 
     Args:
-        number (strorint): the number to be placed in the center of the chip
+        days (strorint): the number to be placed in the center of the chip
 
     """
-    def __init__(self, number: str or int):
-        self.number: str = f"{number}"
+    def __init__(self, days: str or int):
+        self.days: int = int(days)
+        self.number: str = f"{days}"
         self.path: str = f"Images/temp_{get_random_string()}.png"
 
         self._make()
     
     def _make(self) -> None:
         """
-        Creates the temporaty image at Chip.path
+        Creates a temporary image to be used
         """
-        image = Image.open("Images/sobriety_chip.png")
-
-        draw = ImageDraw.Draw(image)
-        w, h = draw.textsize(self.number, font=FONT)
-        x, y = (image.size[0] - w) / 2, (image.size[1] - h) / 2 - 10
-        draw.text((x, y), self.number, fill=FONT_COLOR, font=FONT)
+        if (self.days <= 365):
+            image = Image.open(self._get_premade_image())
+        else:
+            image = Image.open("Images/sobriety_chip.png")
+            draw = ImageDraw.Draw(image)
+            w, h = draw.textsize(self.number, font=FONT)
+            x, y = (image.size[0] - w) / 2, (image.size[1] - h) / 2 - 10
+            draw.text((x, y), self.number, fill=FONT_COLOR, font=FONT)
 
         image.save(self.path)
+
+    def _get_premade_image(self):
+        """
+        Returns the path to the appropriate chip image, based on the number of days
+        """
+        keep = None
+        for image in os.listdir("Images/Chips"):
+            image_days = int(image.split("-")[1].replace(".png", ""))
+            
+            if image_days > self.days:
+                continue
+
+            if keep == None or image_days > keep:
+                keep = image_days
+        
+        return os.path.join("Images/Chips", f"Chip-{keep}.png")
     
     def delete(self) -> None:
         """
